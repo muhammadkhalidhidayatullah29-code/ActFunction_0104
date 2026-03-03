bec673107ea2509606a8979a47cfb7aac06c3495
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-03-03 19:44:40 +0700

    Fungsi Proses

--- Luas_Lingkaran.cpp
+++ Luas_Lingkaran.cpp
@@ -9,3 +9,16 @@ void input(){
     cout << "Masukan jari-jari = ";
     cin >> r;
 }
+// fungsi proses
+float luas_lingkaran (float phi, float r) {
+    return phi * r * r;
+}
+
+void output (){
+    cout << "Luas lingkaran tersebut = " << luas_lingkaran(phi, r) << endl;
+
+}
+int main(){
+    input();
+    output();
+}
